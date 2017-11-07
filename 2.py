e43a9a727c0f2d241d97389b7fb6bb806c36ebe7
Jason Lyn <jason.lyn.lol@gmail.com>
2017-11-07 15:34:21 +1100

    creating files

--- 2.py
+++ 2.py
@@ -2,6 +2,8 @@ import os
 
 a = '/home/lucy/Documents/code/python/exercise' 
 os.chdir(a)
+os.mknod("dummy.txt") #creating files
+os.system("touch dummy2.txt") #creating files
 print os.listdir(os.getcwd())
 
 for f in os.listdir(a):
